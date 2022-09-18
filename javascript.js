7bb3862779a3dfeb313ac70d776a7ca19c71fddf
muzamil098 <61863048+muzamil098@users.noreply.github.com>
2022-09-18 17:51:31 +0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,64 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".navbar", {duration: 1, y:-50});
+
+gsap.from(".top-banner p, .top-banner h2", {duration: 3, opacity:0})
+
+gsap.from(".top-banner button", {
+    duration:1, 
+    opacity:0,
+    y:-100,
+    ease: "elastic.inOut(1, 0.3)"
+})
+gsap.from("#counter span, #counter p", {opacity:0, duration:2, })
+
+gsap.from(".pride, .text-sec, .aboutimg", {
+    scrollTrigger: {
+        trigger: "#about",
+        start: "top 60%",
+        end: "top 50%",
+        scrub: true,
+        markers: true
+      },
+      duration:5 ,
+    opacity:0,
+})
+
+gsap.from(".ecards,#Explore p, #Explore h2", {
+    scrollTrigger: {
+        trigger: "#Explore",
+        start: "top 60%",
+        end: "top 20%",
+        scrub: true,
+        markers: true
+      },
+      duration:5 ,
+          opacity:0,
+  
+})
+
+gsap.from(".stomach,.storypara, #story button", {
+    scrollTrigger: {
+        trigger: "#story",
+        start: "top 70%",
+        end: "top 20%",
+        scrub: true,
+        markers: true
+      },
+        duration:5,
+        opacity:0,
+})
+
+
+gsap.from("#footer", {
+    scrollTrigger: {
+        trigger: "#footer",
+        start: "top 90%",
+        end:"bottom 100%",
+        scrub: true,
+        markers: true
+      },
+        duration:5,
+        opacity:0,
+})
+
